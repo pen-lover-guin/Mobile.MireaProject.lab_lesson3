dbac7bc4977e4e349c800b7d21256851eb593b8c
pen.lover.guin <yana-pichienko@mail.ru>
2019-11-14 16:30:08 +0300

    Add 4 lab: music

--- app/src/main/java/com/example/mireaproject/MainActivity.java
+++ app/src/main/java/com/example/mireaproject/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.mireaproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -67,5 +68,13 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+    public void onClickPlayMusic(View view) {
+        startService(
+                new Intent(MainActivity.this, PlayerService.class));
+    }
+    public void onClickStopMusic(View view) {
+        stopService(
+                new Intent(MainActivity.this, PlayerService.class));
     }
 }
