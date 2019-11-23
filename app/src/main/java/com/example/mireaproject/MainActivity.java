3266d29c70c4d637412998036581092917f2cdad
pen.lover.guin <yana-pichienko@mail.ru>
2019-11-23 11:12:29 +0300

    Add Photo, MediaRecord

--- app/src/main/java/com/example/mireaproject/MainActivity.java
+++ app/src/main/java/com/example/mireaproject/MainActivity.java
@@ -1,46 +1,55 @@
 package com.example.mireaproject;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
+import android.view.Menu;
 import android.view.View;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
-
-import androidx.drawerlayout.widget.DrawerLayout;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.view.Menu;
-import android.widget.EditText;
-import android.widget.TextView;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
-
-    private AppBarConfiguration mAppBarConfiguration;
+    private AppBarConfiguration     mAppBarConfiguration;
+    private  FloatingActionButton fab;
+    private static boolean isPlay = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // инициализация объекта MediaRecorder
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
+        fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                if(!isPlay){
+                Snackbar.make(view, "Playing....", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                    startService(
+                            new Intent(MainActivity.this, PlayerService.class));
+                    isPlay = true;
+            }else {
+                    isPlay = false;
+                    Snackbar.make(view, "Stop....", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                    fab.setImageResource(R.drawable.ic_stop_black_24dp);
+                    stopService(
+                            new Intent(MainActivity.this, PlayerService.class));
+                }
             }
+
         });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
@@ -48,13 +57,16 @@ public class MainActivity extends AppCompatActivity {
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
-                R.id.nav_tools, R.id.nav_share, R.id.nav_send, R.id.nav_brouser)
+                R.id.nav_tools, R.id.nav_share, R.id.nav_send, R.id.nav_brouser, R.id.nav_photo, R.id.nav_media_record)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
     }
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -63,18 +75,12 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+
+
     @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
-    }
-    public void onClickPlayMusic(View view) {
-        startService(
-                new Intent(MainActivity.this, PlayerService.class));
-    }
-    public void onClickStopMusic(View view) {
-        stopService(
-                new Intent(MainActivity.this, PlayerService.class));
     }
 }
